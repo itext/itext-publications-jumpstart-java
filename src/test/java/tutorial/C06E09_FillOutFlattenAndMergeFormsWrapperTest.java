d8d80bfc75a8e687bd95ae110db503805ffcade9
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-09-09 11:05:21 +0300

    Add samples category to the jumpstart tutorial test runner

--- src/test/java/tutorial/C06E09_FillOutFlattenAndMergeFormsWrapperTest.java
+++ src/test/java/tutorial/C06E09_FillOutFlattenAndMergeFormsWrapperTest.java
@@ -3,14 +3,17 @@ package tutorial;
 import com.itextpdf.kernel.utils.CompareTool;
 import com.itextpdf.test.RunnerSearchConfig;
 import com.itextpdf.test.WrappedSamplesRunner;
+import com.itextpdf.test.annotations.type.SampleTest;
 import java.util.Collection;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runners.Parameterized;
 
 /**
  * C06E09_FillOutFlattenAndMergeForms sample has irregular DEST files: it has two of them,
  * so we need process it in a specific way.
  */
+@Category(SampleTest.class)
 public class C06E09_FillOutFlattenAndMergeFormsWrapperTest extends WrappedSamplesRunner {
     @Parameterized.Parameters(name = "{index}: {0}")
     public static Collection<Object[]> data() {
